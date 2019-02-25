8659d5eac72747e3845f5c6323415d40f59e0ff3
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-25 20:59:16 +0000

    build based on 5b01fee

--- dev/search_index.js
+++ dev/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Installation instructions",
     "category": "section",
-    "text": "Oceananigans is still not an official Julia package. But you can install it using the built-in package manager (accessed by pressing ] in the Julia command prompt)julia>]\n(v1.1) pkg> developNote: We recommend using Julia 1.1 with Oceananigans."
+    "text": "Oceananigans is still not an official Julia package. But you can install it using the built-in package manager (accessed by pressing ] in the Julia command prompt)julia>]\n(v1.1) pkg> develop https://github.com/ali-ramadhan/Oceananigans.jl.git\n(v1.1) pkg> test OceananigansNote: We recommend using Julia 1.1 with Oceananigans."
 },
 
 {
