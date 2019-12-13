89a5d52fb008c88ab4f9a188e350a4facbd90ad3
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-13 18:06:41 -0500

    Fix typo in halo_regions.jl

--- src/halo_regions.jl
+++ src/halo_regions.jl
@@ -65,8 +65,8 @@ end
 @inline bottom_gradient(bc::VBC, c¹, Δ, i, j, args...) = ( c¹ - getbc(bc, i, j, args...) ) / (Δ/2)
 @inline top_gradient(bc::VBC, cᴺ, Δ, i, j, args...) =    ( getbc(bc, i, j, args...) - cᴺ ) / (Δ/2)
 
-@inline left_gradient(bc::VBC, c¹, Δ, i, k, args...) =  ( c¹ - getbc(bc, i, k, args...) ) / (Δ/2)
-@inline right_gradient(bc::VBC, cᴺ, Δ, i, k, args...) = ( getbc(bc, i, k, args...) - cᴺ ) / (Δ/2)
+@inline south_gradient(bc::VBC, c¹, Δ, i, k, args...) =  ( c¹ - getbc(bc, i, k, args...) ) / (Δ/2)
+@inline north_gradient(bc::VBC, cᴺ, Δ, i, k, args...) = ( getbc(bc, i, k, args...) - cᴺ ) / (Δ/2)
 
 function fill_bottom_halo!(c, bc::Union{VBC, GBC}, arch, grid, args...)
     @launch device(arch) config=launch_config(grid, :xy) _fill_bottom_halo!(c, bc, grid, args...)
